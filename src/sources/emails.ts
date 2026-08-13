dfe8224ba59f0cea38a24dd5ab256e48f9bc20c5
some-du6e <103129948+some-du6e@users.noreply.github.com>
2026-08-12 22:13:41 -0600

    feat(srcs): work on email a bit

--- src/sources/emails.ts
+++ src/sources/emails.ts
@@ -2,4 +2,38 @@
 // get old emails from old slackscan messages
 // perhaps the same with names and stuff
 // old slackscan messages are REALLY powerful
-// i think its how i got exposed for slack bot :dumbass:
+// i think its how i got exposed for slackbot :dumbass:
+import { App } from "@slack/bolt"
+
+function renderEmailHistory(emails: { email: string; source: string; source_url?: string }[]) {
+    let blocks = []
+
+    blocks.push({
+	    "type": "markdown",
+		"text": "## :email: **"+ emails.length + "** emails found"
+	})
+
+
+
+    return blocks
+}
+
+
+
+export function getEmailHistory(app: App) {
+    let emails = [
+        {
+            "email": "new@example.com",
+            "source": "direct",
+            "ts_gotten": 6767676767
+        },
+        {
+            "email": "old@example.com",
+            "source": "history",
+            "source_url": "https://hackclub.slack.com/archives/xxxxx"
+        }
+    ]
+
+
+
+}
